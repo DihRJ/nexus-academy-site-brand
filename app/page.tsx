8e37322d71a8725a04e1af8cdeb65780dd0eea18
DihRJ <dih.al@hotmail.com>
2025-08-29 12:57:43 -0700

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,6 @@
+export default function Home() {
+  return (<main style={{padding:'2rem',color:'#e2e8f0',background:'#0b1220'}}>
+    <h1>Nexus Academy</h1>
+    <p>Site institucional (reconstruído rapidamente). Faça o build completo usando o pacote entregue anteriormente.</p>
+  </main>);
+}
